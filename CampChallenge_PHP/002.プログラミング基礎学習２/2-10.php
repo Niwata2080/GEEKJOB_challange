0cdda0918fe173812b2d91fce54c09901afda650
Niwata <niwatayuiko@gmail.com>
2017-01-29 10:57:42 +0900

    素因数分解ちょっと改良

--- CampChallenge_PHP/002.プログラミング基礎学習２/2-10.php
+++ CampChallenge_PHP/002.プログラミング基礎学習２/2-10.php
@@ -1,16 +1,30 @@
 <?php
     $input = $_GET['param1'];
     $num = $input;
-    echo $input . '　＝　';
-    //約数は２、３、５、７　それぞれで割り切れなくなるまで割る　その都度出力
-    $prime = array(2, 3, 5, 7);
-    foreach ($prime as $value){
-        while ($num % $value == 0) {
-            $num = $num / $value;
-            echo $value . '×';
+    $output = $input . '　＝　';
+    //追加：例外の処理
+    switch ($num) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 5:
+        case 7:
+            $output .= $num;
+            break;
+        default:
+    //約数は２、３、５、７　それぞれで割り切れなくなるまで割る
+            $prime = array(2, 3, 5, 7);
+            foreach ($prime as $value){
+                while ($num % $value == 0) {
+                    $num = $num / $value;
+                    $output .= $value . '×';
+                }
+            }
         }
-    }
     if ($num > 7){
-        echo 'その他';
+        $output .= 'その他';
     }
-    //最後の×が残ってしまう　要改良：str_replace関数？
+    //最後が×で終わってしまった時対策 効かない？
+    rtrim($output, '×');
+    echo $output;
